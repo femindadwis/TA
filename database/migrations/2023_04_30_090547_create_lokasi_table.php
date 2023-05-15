7751b23c3bc345e6ecfd95e64c4bd35d8f1d33fc
Feminda <93368074+femindadwis@users.noreply.github.com>
2023-05-15 18:24:56 +0700

    ngedit kekurangan database

--- database/migrations/2023_04_30_090547_create_lokasi_table.php
+++ database/migrations/2023_04_30_090547_create_lokasi_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\User;
 
 return new class extends Migration
 {
@@ -16,9 +17,11 @@ return new class extends Migration
         Schema::create('lokasi', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->foreignIdFor(User::class);
             $table->string('alamat');
             $table->string('lng');
             $table->string('lat');
+            $table->string('foto');
             $table->timestamps();
         });
     }
