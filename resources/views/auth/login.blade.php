227bf9c7666057dfce290af8180cc65b3dfe9e54
Feminda <93368074+femindadwis@users.noreply.github.com>
2023-08-03 23:43:27 +0700

    finishing

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,8 +8,8 @@
         <meta name="description" content="viho admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities." />
         <meta name="keywords" content="admin template, viho admin template, dashboard template, flat admin template, responsive admin template, web app" />
         <meta name="author" content="pixelstrap" />
-        <link rel="icon" href="{{ asset('template_dashboard/assets/images/favicon.png') }}" type="image/x-icon" />
-        <link rel="shortcut icon" href="{{ asset('template_dashboard/assets/images/favicon.png') }}" type="image/x-icon" />
+        <link rel="icon" href="{{ asset('template_dashboard/assets/images/login/indramayu.jpg') }}" type="image/x-icon" />
+        <link rel="shortcut icon" href="{{ asset('template_dashboard/assets/images/login/indramayu.jpg') }}" type="image/x-icon" />
         <title>LOGIN
 </title>
         <!-- Google font-->
--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -10,8 +10,8 @@
     <meta name="keywords"
         content="admin template, viho admin template, dashboard template, flat admin template, responsive admin template, web app">
     <meta name="author" content="pixelstrap">
-    <link rel="icon" href="{{ asset('template_dashboard/assets/images/favicon.png') }}" type="image/x-icon">
-    <link rel="shortcut icon" href="{{ asset('template_dashboard/assets/images/favicon.png ') }}" type="image/x-icon">
+    <link rel="icon" href="{{ asset('template_dashboard/assets/images/login/indramayu.jpg') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('template_dashboard/assets/images/login/indramayu.jpg') }}" type="image/x-icon">
     <title>DASHBOARD
 </title>
     <!-- Google font-->
